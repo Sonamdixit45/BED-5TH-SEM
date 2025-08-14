9d175301711397caca2ca20152dfc2edba2741fb
sonam <sonamdixit6686@gmail.com>
2025-08-14 15:40:51 +0530

    done

--- lec-17/model/blog.js
+++ lec-17/model/blog.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema;
 const Blogpost =  new Schema({
     title : String,
     body : String,
-    date : Date
+    date : Date,
+    userId :{
+        type:mongoose.Types.ObjectId,
+        ref:"User"
+    }
 });
  module.exports = mongoose.model('Blogs',Blogpost)
